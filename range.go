79689035e594af14d9da2cc87396a95ac1f12b6b
gocurr <xigua67damn@gmail.com>
2021-09-18 08:56:07 +0800

    Update range.go

--- range.go
+++ range.go
@@ -9,30 +9,32 @@ func Ranges(size, partSize int) []Range {
 		panic("partSize must be greater than 0")
 	}
 
-	if partSize > size {
+	if partSize >= size {
 		return []Range{{From: 0, To: size}}
 	}
 
 	remainder := size % partSize
 	n := size / partSize
+	nn := n
 	if remainder > 0 {
-		n++
+		nn++
 	}
 
-	var ranges = make([]Range, n)
+	var ranges = make([]Range, nn)
 	from := 0
 	to := partSize
-	for i := range ranges {
+	var i int
+	for i = 0; i < n; i++ {
 		// fill ranges with range
 		ranges[i] = Range{From: from, To: to}
-
-		// prepare last item in ranges aka ranges[:-1],
-		// if remainder is bigger than 0,
-		// that means: len(data[:-1]) == remainder
-		if i == n-2 && remainder > 0 {
-			partSize = remainder
-		}
 		from, to = to, to+partSize
+	}
+
+	// prepare last item in ranges aka ranges[:-1],
+	// if remainder is bigger than 0,
+	// that means: len(data[:-1]) == remainder
+	if nn > n {
+		ranges[i] = Range{From: from, To: from + remainder}
 	}
 
 	return ranges
